ca846d4a8d0320a3365e84e55acf728c02ea4421
CodingPeasantZhou <1662467393@qq.com>
2022-12-03 13:02:36 +0800

    zzy 2022/12/3

---
+++ src/components/btn-test.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="tab">
+    <div class="content">
+      <div style="margin: auto;width: 90%">
+        <!--4个按钮-->
+        <button :class="index == number ? 'btn1':'btn'" @click="tab(index)" v-for="(item ,index) in dataList"
+                :key="index">{{item.option}}
+        </button>
+      </div>
+      <!--4个div-->
+      <div id="content1" v-show="number == 0">
+        <span>Java语言：Java是一门面向对象编程语言，不仅吸收了C++语言的各种优点，还摒弃了C++里难以理解的多继承、指针等概念。</span>
+      </div>
+      <div id="content2" v-show="number == 1">
+        <span>C语言：C语言是一门面向过程的、抽象化的通用程序设计语言，广泛应用于底层开发。</span>
+      </div>
+      <div id="content3" v-show="number == 2">
+        <span>JavaScript：是一种具有函数优先的轻量级，解释型或即时编译型的编程语言。</span>
+      </div>
+      <div id="content4" v-show="number == 3">
+        <span>PHP：是在服务器端执行的脚本语言，尤其适用于Web开发并可嵌入HTML中。</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tab",
+  data() {
+    return {
+      number: 0, //点击后的值，与下标同步，为0表示默认第一个按钮与div为选中状态
+      dataList: [
+        {option: 'Java'},
+        {option: 'C+'},
+        {option: 'JavaScript'},
+        {option: 'PHP'},
+      ]
+    }
+  },
+  methods: {
+    //定义切换方法
+    tab(index) {
+      this.number = index;
+      // console.log(index, this.number);
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.content {
+  width: 700px;
+  margin: auto;
+  /*按钮样式*/
+  /*未选中时*/
+  .btn {
+    width: 150px;
+    height: 40px;
+    border: 0;
+    border-radius: 5px;
+    color: white;
+    margin: 20px 3px;
+    background-color: #35cc8f;
+  }
+
+  /*选中时*/
+  .btn1 {
+    width: 150px;
+    height: 40px;
+    border: 0;
+    border-radius: 5px;
+    color: white;
+    margin: 20px 3px;
+    background-color: #099941;
+  }
+
+  #content1, #content2, #content3, #content4 {
+    background-color: #c1f8c9;
+    height: 300px;
+    font-size: 16px;
+    line-height: 40px;
+  }
+}
+</style>
---
+++ src/components/formTest.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-date-picker
+      v-model="formData.startTime"
+      type="date"
+      placeholder="选择日期时间"
+      value-format="yyyy-MM-dd"
+      @change="handleHistoryData">
+  </el-date-picker>
+</template>
+
+<script>
+export default {
+  name: "formTest",
+  data(){
+    return{
+      formData: {
+        startTime: '',// 日期
+      }
+    }
+  },
+  mounted() {
+    this.getCurrentTime()
+  },
+  methods:{
+    // 获取日期
+    handleHistoryData(val) {
+      console.log(val)
+      this.startTime = val
+    },
+    // 获取当前日期
+    getCurrentTime() {
+      let currentTime = new Date(), year = currentTime.getFullYear(),
+          month = currentTime.getMonth() + 1 < 10 ? '0' + (currentTime.getMonth() + 1) : currentTime.getMonth() + 1,
+          day = currentTime.getDate() < 10 ? '0' + currentTime.getDate() : currentTime.getDate()
+      this.formData.startTime = year + '-' + month + '-' + day
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
